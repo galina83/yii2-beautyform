06c98fcc182b0fb015c34eed2d6bf129ad6c7375
galina83 <galina.benedictovich@gmail.com>
2018-07-14 16:18:30 +0300

    My first extension
    Un progress

---
+++ AutoloadExample.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace galina83\beautyform;
+
+/**
+ * This is just an example.
+ */
+class AutoloadExample extends \yii\base\Widget
+{
+    public function run()
+    {
+        return "Hello!";
+    }
+}
